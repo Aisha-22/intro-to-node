ab662aa12f236a0bfa8b63586ee3ef303965b6d0
Aisha <ash.hlatshwayo22@gmail.com>
2019-12-20 22:35:47 +0200

    To be complete

---
+++ index.js
@@ -0,0 +1,10 @@
+// jshint esversion:6
+
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var mySupervillainNames = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(mySupervillainNames);
